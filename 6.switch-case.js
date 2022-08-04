7a8bf4b4a1153123689d9f022a3d22b95d5d2753
bekirglr <bbekirglr@gmail.com>
2022-08-05 00:41:26 +0300

    learned switch case condition

---
+++ 6.switch-case.js
@@ -0,0 +1,29 @@
+/* Switch Case Yapısı
+switch (kontrol edilecek durum){
+    case 1:
+        kod çalıştır;
+        break;
+    case 2:
+        kod çalıştır;
+        break;
+    ...
+    default :
+        kod çalıştır
+}
+*/
+
+var job="Software";
+
+switch (job){
+    case "Doctor":
+        console.log("I'm a doctor");
+        break;
+    case "Teacher":
+        console.log("I'm a teacher");
+        break;
+    case "Software":
+        console.log("I'm a software'");
+        break;
+    default:
+        console.log("İşsizlik");
+}
