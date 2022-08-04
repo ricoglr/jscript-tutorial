6d7f3750969493aac8d9c7e4fe71d8b482d347ca
bekirglr <bbekirglr@gmail.com>
2022-08-05 00:20:25 +0300

    learned variables

---
+++ 1.variables.js
@@ -0,0 +1,16 @@
+
+var name ="Aaron";
+console.log(name);
+
+var age =21;       //number data type
+console.log(age);
+
+var job;           //string data type
+job="Software"
+console.log(job);
+
+var erkek=true;    // boolean data type
+console.log(erkek);
+
+var salary = 4000;  //typeof ile data type ı görme
+console.log(typeof salary);
