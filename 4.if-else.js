6c9bbf6d6a99e4125b34fc301f0d90043b020f32
bekirglr <bbekirglr@gmail.com>
2022-08-05 00:40:35 +0300

    learned conditions

---
+++ 4.if-else.js
@@ -0,0 +1,18 @@
+
+
+var x=4;
+/*if (x>4){
+    console.log("X 4'ten büyüktür.");
+}else{
+    console.log("X 4'ten büyük değildir.");
+}*/
+
+if (x>4){
+    console.log("X 4'ten büyüktür.");
+}
+else if (x<4) {
+    console.log("X 4'ten Küçüktür'");
+}
+else {
+    console.log("X 4'e eşittir'");
+}
