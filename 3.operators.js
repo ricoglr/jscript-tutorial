5fd680812799d30b0dc51a04db7659f34981e681
bekirglr <bbekirglr@gmail.com>
2022-08-05 00:39:45 +0300

    learned operators

---
+++ 3.operators.js
@@ -0,0 +1,43 @@
+// Aritmetik operatörler + - * /
+/*var x = 6;
+console.log(x);
+
+ x = x+4;
+ console.log(x);
+
+ x -= 5;
+ console.log(x);
+
+ x *=10;
+ console.log(x);
+
+ x /=2;
+ console.log(x);*/
+
+
+ // Assigment Operatörler: =, ==, ===
+// === veri tipi olarak eşitliği belirtir
+/* var x = 5;
+ var y = 7;
+
+ if (x == y) {
+  console.log(true)}
+ else {
+  console.log(false);
+ }*/
+
+
+// Modulus Operatörü: %
+/*var year =  2022;
+var kalan =  year % 5;
+console.log(kalan)*/
+
+// Increment Operatörü: ++
+/*var x=4;
+console.log(x++); //yazdırdıktan sonra bir arttırır yani ekranda yine 4 görünür. ama bir artmıştır.
+console.log(++x); //bir arttırdıktan sonra ekrana yazdırır.*/
+
+// Decrement Operatörü: --
+/*var x=4;
+console.log(x++); //yazdırdıktan sonra bir eksiltir yani ekranda yine 4 görünür. ama bir eksilmiştri.
+console.log(++x); //bir eksilttikten sonra ekrana yazdırır.*/
