dae7ee406f6774b61ec0c0602c40f75f51f24232
bekirglr <bbekirglr@gmail.com>
2022-08-08 16:16:16 +0300

    Learned prompt

---
+++ 10.prompt.js
@@ -0,0 +1,10 @@
+// prompt kullanıcıdan veri almayı sağlar
+
+var name = prompt("Adınızı Giriniz :");
+//console.log(name);
+//document.write(name);
+//alert(name);
+
+//prompt ile girilen her değer string ifadedir.
+//console.log(typeof name);
+
